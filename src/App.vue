551fcca02c3959a0aa494aedd5ee22ca7b1fd65e
Cristian David Home <cristian_david033@hotmail.com>
2020-11-25 12:58:44 -0500

    Componente Básico de Vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,73 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <h1>Productos</h1>
+    <table border="1">
+      <thead>
+        <th>id</th>
+        <th>nombre</th>
+        <th>tipo</th>
+        <th>descripcion</th>
+        <th>valorUnidad</th>
+        <th>fechaEntrega</th>
+        <th>fechaVencimiento</th>
+        <th>activo</th>
+        <th>Accion</th>
+      </thead>
+      <tbody>
+        <tr v-for="producto in productos" :key="producto.id">
+          <td>{{ producto.id }}</td>
+          <td>{{ producto.nombre }}</td>
+          <td>{{ producto.tipo }}</td>
+          <td>{{ producto.descripcion }}</td>
+          <td>{{ producto.valorUnidad }}</td>
+          <td>{{ producto.fechaEntrega }}</td>
+          <td>{{ producto.fechaVencimiento }}</td>
+          <td>{{ producto.activo }}</td>
+          <td><button @click="elminarProducto(producto.id)">x</button></td>
+        </tr>
+      </tbody>
+    </table>
+    <button @click="fetchProductos">Recaragar</button>
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: () => ({
+    productos: [],
+  }),
+  computed: {},
+  methods: {
+    fetchProductos: function() {
+      axios
+        .get("http://localhost:8000/contratos/productos/")
+        .then((response) => {
+          this.productos = response.data;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+    elminarProducto: function(id) {
+      axios
+        .delete("http://localhost:8000/contratos/productos/" + id + "/")
+        .then(( ) => {})
+        .catch((err) => {
+          console.log(err);
+        })
+        .finally(() => {
+          this.fetchProductos();
+        });
+    },
+  },
+  created: function() {},
+  mounted: function() {
+    this.fetchProductos();
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
