aa971cfb07cb3077e5f61a7458bb62bab9c8dad2
Cristian David Home <cristian_david033@hotmail.com>
2020-11-27 11:04:37 -0500

    Agregar vuetify.

--- src/App.vue
+++ src/App.vue
@@ -1,93 +1,25 @@
 <template>
-  <div id="app">
-    <h1>Productos</h1>
-    <table border="1">
-      <thead>
-        <th>id</th>
-        <th>nombre</th>
-        <th>tipo</th>
-        <th>descripcion</th>
-        <th>valorUnidad</th>
-        <th>fechaEntrega</th>
-        <th>fechaVencimiento</th>
-        <th>activo</th>
-        <th>Accion</th>
-      </thead>
-      <tbody>
-        <tr v-for="producto in productos" :key="producto.id">
-          <td>{{ producto.id }}</td>
-          <td>{{ producto.nombre }}</td>
-          <td>{{ producto.tipo }}</td>
-          <td>{{ producto.descripcion }}</td>
-          <td>{{ producto.valorUnidad }}</td>
-          <td>{{ producto.fechaEntrega }}</td>
-          <td>{{ producto.fechaVencimiento }}</td>
-          <td>{{ producto.activo }}</td>
-          <td><button @click="elminarProducto(producto.id)">x</button></td>
-        </tr>
-      </tbody>
-    </table>
-    <button @click="fetchProductos">Recaragar</button>
-  </div>
+  <v-app>
+    <v-app-bar app color="primary" dark> </v-app-bar>
+
+    <v-main>
+      <Productos />
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import axios from "axios";
+import Productos from "./components/Productos";
 
 export default {
+  name: "App",
+
+  components: {
+    Productos,
+  },
+
   data: () => ({
-    productos: [],
+    //
   }),
-  computed: {},
-  methods: {
-    fetchProductos: function() {
-      axios
-        .get("http://localhost:8000/contratos/productos/")
-        .then((response) => {
-          this.productos = response.data;
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    },
-    elminarProducto: function(id) {
-      axios
-        .delete("http://localhost:8000/contratos/productos/" + id + "/")
-        .then(( ) => {})
-        .catch((err) => {
-          console.log(err);
-        })
-        .finally(() => {
-          this.fetchProductos();
-        });
-    },
-  },
-  created: function() {},
-  mounted: function() {
-    this.fetchProductos();
-  },
 };
 </script>
-
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
---
+++ src/components/Productos.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container>
+    <v-row class="text-center">
+      <v-col>
+        <h1>Productos</h1>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "Productos",
+  data: () => ({}),
+};
+</script>
